773b65aedfcd2da2bb6a8cd7c3701d1fdf1fe137
jack <jackhuang1205@gmail.com>
2019-12-24 23:44:14 +0800

    Fix python 2 to 3 serial write error

--- face_segmentation/b7_ttl_send.py
+++ face_segmentation/b7_ttl_send.py
@@ -8,6 +8,10 @@ start_time = time.time()
 with serial.Serial('/dev/ttyUSB0', 19200) as ser:
     while True:
         value = input()
-        ser.write(chr(((value >> 14) & 0x0003)))
-        ser.write(chr(((value >> 7) & 0x007F)))
-        ser.write(chr(((value & 0x00FF) | 0x0080)))
+        value = int(value) 
+        upper = ((value >> 14) & 0x0003).to_bytes(1, byteorder='big')
+        middle = ((value >> 7) & 0x007F).to_bytes(1, byteorder='big')
+        low = ((value & 0x00FF) | 0x0080).to_bytes(1, byteorder='big')
+        ser.write(upper)
+        ser.write(middle)
+        ser.write(low)
